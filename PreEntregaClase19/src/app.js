6e08eee33fe4fd2b0f2c0e230a3a7f1ba09d894a
Franco Zandomeni <francozandomeni@gmail.com>
2024-01-11 22:54:39 -0300

    PreEntrega Clase 19. Register y Login, con renderizado de productos luego de inicio de sesion

---
+++ PreEntregaClase19/src/app.js
@@ -0,0 +1,43 @@
+import express from "express"
+import session from "express-session"
+import MongoStore from "connect-mongo"
+import mongoose from "mongoose"
+import __dirname from "./utils.js"
+import { engine } from "express-handlebars"
+import viewRoutes from "./routes/views.router.js"
+import sessionRouter from "./routes/sessions.router.js"
+
+const MONGO = "mongodb+srv://kacozandomeni:Jumzf8owIStzB2bb@cluster0.wtjpkqk.mongodb.net/ecommerce";
+
+const PORT = 8080
+const app = express()
+
+
+const connection = mongoose.connect(MONGO)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+
+app.use(session({
+    store: new MongoStore({
+        mongoUrl: MONGO,
+        ttl:3600
+    }),
+    secret:"CoderSecret",
+    resave:false,
+    saveUninitialized:false
+}))
+
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", `${__dirname}/views`)
+
+app.use("/", viewRoutes)
+app.use("/api/sessions", sessionRouter)
+
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`)
+})
---
+++ PreEntregaClase19/src/config/config.js
@@ -0,0 +1,66 @@
+import passport from "passport";
+import local from "passport-local";
+
+import userModel from "../models/Users.model.js";
+import {createHash, validatePassword} from "../utils.js";
+
+const LocalStrategy = local.Strategy;
+
+const inicializePassport = () => {
+
+    passport.use("register", new LocalStrategy(
+        {passReqToCallback:true, usernameField:"email"},
+        async ( req, username, password, done ) => {
+        const { first_name, last_name, email, age } = req.body;
+        try {
+            
+            let user = await userModel.findOne({email:username});
+            if(user){
+                console.log('Usuario ya registrado');
+                return done(null,false)
+            }
+            const newUser = {
+                first_name,
+                last_name,
+                email,
+                age,
+                password: createHash(password)
+            }
+            const result = await userModel.create(newUser);
+            return done (null, result);
+
+        } catch (error) {
+            return done(error)
+        }    
+
+    }));
+
+    passport.use("login", new LocalStrategy(
+    {usernameField:"email"},
+    async (username, password, done)=>{
+        try {
+            const user = await userModel.findOne({email:username})
+            if(!user){
+                return done(null, false);
+            }
+            if(!validatePassword(password, user)){
+                return done(null, false);
+            } 
+            return done(null,user)
+        } catch (error) {
+            return done(error);
+        }
+    }))
+
+    passport.serializeUser((user,done)=>{
+        done(null, user._id)
+    });
+
+    passport.deserializeUser(async (id,done)=>{
+        let user = await userModel.findById(id);
+        done(null, user);
+    });
+    
+}
+
+export default inicializePassport;
